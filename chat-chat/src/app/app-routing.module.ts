7f1bfd3c30d9c9c937231f9bba932f74f19de094
kshimauchi <kshimauchi@gmail.com>
2020-06-21 13:26:48 -0700

    basic-structure

--- chat-chat/src/app/app-routing.module.ts
+++ chat-chat/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupFormComponent } from '../app/signup-form/signup-form.component';
+import { LoginFormComponent } from '../app/login-form/login-form.component';
+import { ChatRoomComponent } from '../app/chat-room/chat-room.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path: 'signup', component: SignupFormComponent},
+  {path: 'login', component: LoginFormComponent },
+  {path: 'chat', component: ChatRoomComponent },
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+};
+ 
